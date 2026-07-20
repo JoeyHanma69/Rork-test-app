81669ee3cb4215ec98be0acead0643359c10a067
Joseph Linao <128703682+JoeyHanma69@users.noreply.github.com>
2026-07-20 21:35:57 +1000

    Swift

---
+++ ios/TapDodgeTests/TapDodgeTests.swift
@@ -0,0 +1,17 @@
+//
+//  TapDodgeTests.swift
+//  TapDodgeTests
+//
+//  Created by Rork on July 19, 2026.
+//
+
+import Testing
+@testable import TapDodge
+
+struct TapDodgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
